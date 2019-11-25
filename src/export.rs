a790cd5624dd9f5e2b926843c9cc2b8ec5e650b1
Jonas Schievink <jonasschievink@gmail.com>
2019-11-25 11:54:00 +0100

    Ignore errors instead

--- src/export.rs
+++ src/export.rs
@@ -9,64 +9,43 @@ use crate::hio::{self, HStderr, HStdout};
 static mut HSTDOUT: Option<HStdout> = None;
 
 pub fn hstdout_str(s: &str) {
-    let result = interrupt::free(|_| unsafe {
+    let _result = interrupt::free(|_| unsafe {
         if HSTDOUT.is_none() {
             HSTDOUT = Some(hio::hstdout()?);
         }
 
         HSTDOUT.as_mut().unwrap().write_str(s).map_err(drop)
     });
-
-    if result.is_err() {
-        error("hstdout");
-    }
 }
 
 pub fn hstdout_fmt(args: fmt::Arguments) {
-    let result = interrupt::free(|_| unsafe {
+    let _result = interrupt::free(|_| unsafe {
         if HSTDOUT.is_none() {
             HSTDOUT = Some(hio::hstdout()?);
         }
 
         HSTDOUT.as_mut().unwrap().write_fmt(args).map_err(drop)
     });
-
-    if result.is_err() {
-        error("hstdout");
-    }
 }
 
 static mut HSTDERR: Option<HStderr> = None;
 
 pub fn hstderr_str(s: &str) {
-    let result = interrupt::free(|_| unsafe {
+    let _result = interrupt::free(|_| unsafe {
         if HSTDERR.is_none() {
             HSTDERR = Some(hio::hstderr()?);
         }
 
         HSTDERR.as_mut().unwrap().write_str(s).map_err(drop)
     });
-
-    if result.is_err() {
-        error("hstderr");
-    }
 }
 
 pub fn hstderr_fmt(args: fmt::Arguments) {
-    let result = interrupt::free(|_| unsafe {
+    let _result = interrupt::free(|_| unsafe {
         if HSTDERR.is_none() {
             HSTDERR = Some(hio::hstderr()?);
         }
 
         HSTDERR.as_mut().unwrap().write_fmt(args).map_err(drop)
     });
-
-    if result.is_err() {
-        error("hstderr");
-    }
-}
-
-#[cold]
-fn error(label: &str) {
-    panic!("failed to print to {}", label);
 }
